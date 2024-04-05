dbd2e0336efd8503bcb139521e9e845981a4fbc1
Rodrigo Carvalho <142619236+skrodrigo@users.noreply.github.com>
2024-04-05 14:36:59 -0300

    Add files via upload

---
+++ peton-site/script.js
@@ -0,0 +1,10 @@
+let btnT= document.getElementsByClassName("btn-wait-list-top");
+btnT[0].addEventListener("click", click);
+function click() {
+  alert("Obrigado por se inscrever na lista de espera!");
+}
+let btnD= document.getElementsByClassName("btn-wait-list-down");
+btnD[0].addEventListener("click", click);
+function click() {
+  alert("Obrigado por se inscrever na lista de espera!");
+}
